6624be082cb80b0aaa4eeb56c51cf38a36211da5
talha <bukhars@purdue.edu>
2025-02-07 11:38:23 -0500

    fix

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,3 +7,10 @@ import yaml
 def write_yaml_file(data: Dict, filepath: Path):
     with open(filepath, "w") as f:
         yaml.dump(data, f, default_flow_style=False)
+
+
+def parse_yaml_file(filepath: Path):
+    assert filepath.exists(), f"Non-existent yaml file: {filepath}"
+    with open(filepath, "r") as f:
+        data = yaml.safe_load(f)
+    return data
